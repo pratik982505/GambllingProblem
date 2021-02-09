80e1f9e75a6fea27a7be80b7c43df48dafe9d76a
pratik982505 <gharatp%5@gmail.com>
2021-02-09 23:50:13 +0530

    PratikGharat Added : code for condtion Win or loss

--- Gambling.js
+++ Gambling.js
@@ -3,4 +3,18 @@ console.log("Wlcome to Gambling Simulator");
 let Stake = 100;
 const bet = 1;
 console.log("The Player has Total : "+"$"+ Stake +  " " +"stake");
-
+    gamble = () => {
+        let Bet = Math.floor(Math.random()*2);
+        switch (Bet){
+            case 0 :
+                Stake++;
+           console.log("Player won.\tNow Player has Total: "+"$" + Stake);
+            break;
+            case 1 :
+                Stake--;
+            console.log("Player Loss.\tNow player has Total:"+"$"+ Stake);
+             break;
+        }
+    }
+    
+   console.log(gamble());
