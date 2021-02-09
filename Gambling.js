ad7b2191574ed8d80555379e0793a0c4a15ccd39
pratik982505 <gharatp%5@gmail.com>
2021-02-10 00:00:00 +0530

    PratikGharat Added Code for Resign Condition when Stake reach 50% more or less

--- Gambling.js
+++ Gambling.js
@@ -2,10 +2,18 @@ console.log("Wlcome to Gambling Simulator");
 
 let Stake = 100;
 const bet = 1;
-console.log("The Player has Total : "+"$"+ Stake +  " " +"stake");
-    gamble = () => {
-        let Bet = Math.floor(Math.random()*2);
-        switch (Bet){
+const Wstake =150;
+const Lstake=50;
+let flag = true;
+console.log("The Player has Total : "+"$"+ Stake + "stake");
+
+class Gamblling{
+  gamble = () => {
+    while(flag){
+
+      let Bet = Math.floor(Math.random()*2);
+       
+       switch (Bet){
             case 0 :
                 Stake++;
            console.log("Player won.\tNow Player has Total: "+"$" + Stake);
@@ -14,7 +22,17 @@ console.log("The Player has Total : "+"$"+ Stake +  " " +"stake");
                 Stake--;
             console.log("Player Loss.\tNow player has Total:"+"$"+ Stake);
              break;
-        }
+        } 
+          this.resign();
     }
-    
-   console.log(gamble());
+}
+  resign() {
+      if (Stake>=Wstake || Stake<= Lstake){
+          console.log("\n Resign for Day and" +" "+"$"+Stake);
+    flag = false;
+      }
+   } 
+} 
+  var opt = new Gamblling();
+
+  console.log(opt.gamble());
