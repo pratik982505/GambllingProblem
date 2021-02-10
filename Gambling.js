d93adee58f4db3be6b63e96eefa0d5cdfd1d478a
pratik982505 <gharatp%5@gmail.com>
2021-02-10 08:07:49 +0530

    PratikGharat Modify : Code for Amount After 20Days

--- Gambling.js
+++ Gambling.js
@@ -2,37 +2,50 @@ console.log("Wlcome to Gambling Simulator");
 
 let Stake = 100;
 const bet = 1;
+let totalAmount = 300;
+let totalStake =0;
 const Wstake =150;
 const Lstake=50;
 let flag = true;
+const DAYS = 3;
+let StakeArray = [];
 console.log("The Player has Total : "+"$"+ Stake + "stake");
 
 class Gamblling{
   gamble = () => {
+    for (let day=1; day<=DAYS; day++){
+      Stake=100;
+      flag=true;
+
     while(flag){
 
       let Bet = Math.floor(Math.random()*2);
-       
-       switch (Bet){
-            case 0 :
+       if (Bet ==bet){
                 Stake++;
-           console.log("Player won.\tNow Player has Total: "+"$" + Stake);
-            break;
-            case 1 :
-                Stake--;
-            console.log("Player Loss.\tNow player has Total:"+"$"+ Stake);
-             break;
-        } 
-          this.resign();
+       }
+        else{
+         Stake --;
+        }
+          this.resign(day);
     }
 }
-  resign() {
+if  (totalAmount > totalStake){
+      console.log("Total Ammount loss"+"$"+(totalAmount-totalStake));
+       }
+        else{
+                console.log("Total Ammount profit" + "$" + (totalStake-totalAmount));
+        }
+    }
+    resign(day) {
       if (Stake>=Wstake || Stake<= Lstake){
-          console.log("\n Resign for Day and" +" "+"$"+Stake);
-    flag = false;
+console.log("\n On day " + " " + day +" "+ "Player Resign for the day with" +"$" + Stake);
+StakeArray[day-1] = Stake;
+totalStake += Stake;
+flag = false;
       }
-   } 
-} 
+    }
+  }
+
   var opt = new Gamblling();
 
   console.log(opt.gamble());
